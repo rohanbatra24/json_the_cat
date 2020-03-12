d10eca6f567a6ad9c4af9cb1abbfcd03b0347c51
Rohan Batra <rohanbatra24@gmail.com>
2020-03-12 19:02:38 +0000

    added tests and modularized code with index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,19 @@
 const request = require('request');
 
-const breed = process.argv[2];
+const fetchBreedDescription = function(breedName, callback) {
+  const link = `http://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(link, (error, response, body) => {
+    if (error && error.code === 'ENOTFOUND') {
+      callback('URL not found', null);
+    } else if (error || body[0] === undefined) {
+      callback('Not found', null);
+      return;
+    } else {
+      const data = JSON.parse(body);
+      callback(null, data[0].description.trim());
+      return;
+    }
+  });
+};
 
-const link = `http://api.thecatapi.com/v1/breeds/search?q=${breed}`;
-
-request(link, (error, _response, body) => {
-  if (error && error.code === 'ENOTFOUND') {
-    console.log('URL not found');
-  } else if (error || body[0] === undefined) {
-    console.error('Not found');
-    return;
-  } else {
-    const data = JSON.parse(body);
-    console.log(data[0].description);
-    return;
-  }
-});
+module.exports = { fetchBreedDescription };
